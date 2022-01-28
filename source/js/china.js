6060bbc6ae6afac65f58ac93386651881acf43b1
Fgaoxing <Fgaoxing0206@163.com>
2022-01-28 09:48:01 +0800

    Update china.js

--- source/js/china.js
+++ source/js/china.js
@@ -25,3 +25,26 @@
         head.appendChild(styleNode);
     }
 })();
+//判断是春节，显示小灯笼
+(function () {
+    var date=new Date();
+    var month =date.getMonth();//0~11
+    var day = date.getDate();//1-31
+
+    if(month<=1){
+        var styleNode = document.createElement('script');
+        styleNode['src'] = 'https://api.vvhan.com/api/denglong' ;
+        var cssText = `
+`;
+
+        if(styleNode.styleSheet){
+            styleNode.styleSheet.cssText = cssText;
+        }
+        else{
+            styleNode.appendChild(document.createTextNode(cssText));
+        }
+
+        var head = document.getElementsByTagName('head')[0] ;
+        head.appendChild(styleNode);
+    }
+})();
