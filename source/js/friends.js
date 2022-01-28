66f19fe14bfc7144a36ac20f8392314a7d262517
Fgaoxing <Fgaoxing0206@163.com>
2022-01-28 19:55:25 +0800

    Create friends.js

---
+++ source/js/friends.js
@@ -0,0 +1,44 @@
+function loadQexoFriends(id, url) {
+    var uri = url + "/pub/friends/";
+    var loadStyle = '<div class="qexo_loading"><div class="qexo_part"><div style="display: flex; justify-content: center"><div class="qexo_loader"><div class="qexo_inner one"></div><div class="qexo_inner two"></div><div class="qexo_inner three"></div></div></div></div><p style="text-align: center; display: block">友链加载中...</p></div>';
+    document.getElementById(id).className = "qexo-friends";
+    document.getElementById(id).innerHTML = loadStyle;
+    var ajax;
+    try {
+        // Firefox, Opera 8.0+, Safari
+        ajax = new XMLHttpRequest();
+    } catch (e) {
+        // Internet Explorer
+        try {
+            ajax = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                ajax = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert("糟糕,你的浏览器不能上传文件!");
+                return false;
+            }
+        }
+    }
+    ajax.open("get", uri, true);
+    ajax.setRequestHeader("Content-Type", "text/plain");
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
+                var res = JSON.parse(ajax.response);
+                if (res["status"]) {
+                    var friends = res["data"];
+                    document.getElementById(id).innerHTML = "";
+                    for (let i = 0; i < friends.length; i++) {
+                        document.getElementById(id).innerHTML += '<li><a class="flat-box" title="' + friends[i]["name"] + ' target="_blank" rel="noopener" href="' + friends[i]["url"] + '" active-action="action-' + friends[i]["name"] + '"><div class="name"><img class="lazyload loaded" alt="This is a picture without description" src="' + friends[i]["imag"] + ' srcset="' + friends[i]["image"] + '" data-ll-status="loaded">' + friends[i]["name"] + '</div></a></li>'; 
+                    }
+                } else {
+                    alert("友链载入失败!");
+                }
+            } else {
+                alert("友链获取失败!");
+            }
+        }
+    };
+    ajax.send(null);
+}
