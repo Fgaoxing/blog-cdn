c6717d53ab7e5f8eb89f0231912c80e17875e27f
Fgaoxing <Fgaoxing0206@163.com>
2022-01-28 21:51:43 +0800

    up

--- source/js/f.js
+++ source/js/f.js
@@ -1,8 +1,8 @@
 function loadQexoFriends(id, url) {
     var uri = url + "/pub/friends/";
     var loadStyle = '<div class="qexo_loading"><div class="qexo_part"><div style="display: flex; justify-content: center"><div class="qexo_loader"><div class="qexo_inner one"></div><div class="qexo_inner two"></div><div class="qexo_inner three"></div></div></div></div><p style="text-align: center; display: block">友链加载中...</p></div>';
-    document.getElementByClassName(id).className = "list entry navigation";
-    document.getElementByClassName(id).innerHTML = loadStyle;
+    document.getElementById(id).className = "list entry navigation";
+    document.getElementById(id).innerHTML = loadStyle;
     var ajax;
     try {
         // Firefox, Opera 8.0+, Safari
@@ -28,9 +28,9 @@ function loadQexoFriends(id, url) {
                 var res = JSON.parse(ajax.response);
                 if (res["status"]) {
                     var friends = res["data"];
-                    document.getElementByClassName(id).innerHTML = "";
+                    document.getElementById(id).innerHTML = "";
                     for (let i = 0; i < friends.length; i++) {
-                        document.getElementByClassName(id).innerHTML += '<li><a class="flat-box" title="' + friends[i]["name"] + ' target="_blank" rel="noopener" href="' + friends[i]["url"] + '" active-action="action-' + friends[i]["name"] + '"><div class="name"><img class="lazyload loaded" alt="哎呀，图片丢了" src="' + friends[i]["image"] + '" srcset="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-ll-status="loaded">' + friends[i]["name"] + '</div></a></li>'; 
+                        document.getElementById(id).innerHTML += '<li><a class="flat-box" title="' + friends[i]["name"] + ' target="_blank" rel="noopener" href="' + friends[i]["url"] + '" active-action="action-' + friends[i]["name"] + '"><div class="name"><img class="lazyload loaded" alt="哎呀，图片丢了" src="' + friends[i]["image"] + '" srcset="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-ll-status="loaded">' + friends[i]["name"] + '</div></a></li>'; 
                     }
                 } else {
                     alert("友链载入失败!");
