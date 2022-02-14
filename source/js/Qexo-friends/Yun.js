8ca916f29ac83a57949565140f22221a654ae7e6
Fgaoxing <92655031+Fgaoxing@users.noreply.github.com>
2022-02-14 11:37:13 +0800

    up yun

---
+++ source/js/Qexo-friends/Yun.js
@@ -0,0 +1,45 @@
+function loadQexoFriends(id, url, color) {
+    var uri = url + "/pub/friends/";
+    var loadStyle = '<div class="qexo_loading"><div class="qexo_part"><div style="display: flex; justify-content: center"><div class="qexo_loader"><div class="qexo_inner one"></div><div class="qexo_inner two"></div><div class="qexo_inner three"></div></div></div></div><p style="text-align: center; display: block">友链加载中...</p></div>';
+    document.getElementById(id).className = "Qexo-friends";
+    document.getElementById(id).innerHTML = loadStyle;
+    var ajax;
+    try {
+        // Firefox, Opera 8.0+, Safari
+        ajax = new XMLHttpRequest();
+    } catch (e) {
+        // Internet Explorer
+        try {
+            ajax = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                ajax = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert("糟糕,你的浏览器不能上传文件!");
+                return false;
+            }
+        }
+    }
+    ajax.open("get", uri, true);
+    ajax.setRequestHeader("Content-Type", "text/plain");
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
+                var res = JSON.parse(ajax.response);
+                if (res["status"]) {
+                    var friends = res["data"];
+                    document.getElementById(id).innerHTML = '<ul class="link-items">';
+                    for (let i = 0; i < friends.length; i++) {
+                        document.getElementById(id).innerHTML += '<li class="link-item" id="' + friends[i]["name"] + '" style="--primary-color:'+color+'"><a class="link-url" href="' + friends[i]["url"] + '" title="' + friends[i]["description"] + '" alt="portrait" target="_blank" rel="friend"><div class="link-left"><img class="link-avatar" alt="哎呀，图片丢了" loading="lazy" src="' + friends[i]["image"] + '" onerror="onAvatarError(this)"></div><div class="link-info"><div class="link-blog">' + friends[i]["name"] + '</div><div class="link-desc">' + friends[i]["description"] + '</div></div></a></li>';
+                    }
+                    document.getElementById(id).innerHTML += '</ul><br>';
+                } else {
+                    alert("友链载入失败!");
+                }
+            } else {
+                alert("友链获取失败!");
+            }
+        }
+    };
+    ajax.send(null);
+}
