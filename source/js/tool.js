c355271fd0c2d15abe9835b7c27823a7c5cfaf59
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-03-31 09:56:11 +0800

    Update tool.js

--- source/js/tool.js
+++ source/js/tool.js
@@ -8,3 +8,20 @@ function MirrorProtection(url) {
 	window.location.href='https://www.yt-blog.top/MirrorProtection/?url='+url+'&uri='+uri;
 }
 };
+//判断是国家公祭日12.13，加载css样式。
+function Memorial(){
+    var date=new Date();
+    if(date.getMonth()===2&&date.getDate()===31){
+        document.getElementsByTagName('head')[0].innerHTML += '<style type="text/css">html {filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);-webkit-filter: grayscale(1);}<style>'
+}};
+//判断是春节，显示小灯笼
+function year(){
+    var date=new Date();
+    if(date.getMonth()<2){
+        document.getElementsByTagName('head')[0].innerHTML += '<script src="https://api.vvhan.com/api/denglong"><script>'
+}};
+
+        var head = document.getElementsByTagName('head')[0] ;
+        head.appendChild(styleNode);
+    }
+})();
